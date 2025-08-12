7063682133a399d1ebbff0ed55819c33047c7fbc
achnitreda <achnitreda@gmail.com>
2025-08-12 11:36:40 +0100

    helloWorld

--- HelloWorld/HelloWorld.java
+++ HelloWorld/HelloWorld.java
@@ -1,8 +1,8 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println(HelloWorld.HelloWorld());
+        System.out.println(HelloWorld.helloWorld());
     }
-    public static String HelloWorld() {
+    public static String helloWorld() {
         return "Hello World !";
     }
 }
