780259b038c9cc334cd1b10fbcd7f15dc16ba26f
achnitreda <achnitreda@gmail.com>
2025-08-14 08:20:29 +0100

    catinFile

--- CatInFile/CatInFile.java
+++ CatInFile/CatInFile.java
@@ -2,14 +2,14 @@ import java.io.*;
 
 public class CatInFile {
     public static void cat(String[] args) throws IOException {
-        if (args == null || args.length == 0) return;
-
-        try (InputStream input = System.in;
-        FileOutputStream output = new FileOutputStream(args[0])) {
-            int b;
-            while ((b = input.read()) != -1) {
-                output.write(b);
-            }
+        if (args == null || args.length == 0) {
+            return;
+        }
+        byte[] data = System.in.readAllBytes();
+        String file = args[0];
+        try (OutputStream inputStream = new FileOutputStream(file)) {
+            inputStream.write(data);
+            inputStream.close();
         }
     }
 }
