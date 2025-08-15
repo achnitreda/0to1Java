9af7bdeb4b86f0cea9bdce3fbb8e0338e6ccd700
achnitreda <achnitreda@gmail.com>
2025-08-15 17:43:30 +0100

    getters

---
+++ StarGetters/CelestialObject.java
@@ -0,0 +1,46 @@
+public class CelestialObject {
+    public double x,y,z;
+    public String name;
+
+    public CelestialObject() {
+        this.x = 0.0;
+        this.y = 0.0;
+        this.z = 0.0;
+        this.name = "Soleil";
+    }
+
+    public CelestialObject(String name, double x, double y, double z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+        this.name = name;
+    }
+
+    public double getX(){
+        return this.x;
+    }
+    public double getY(){
+        return this.y;
+    }
+    public double getZ(){
+        return this.z;
+    }
+    public void setX(double x){
+        this.x = x;
+    }
+    public void setY(double y){
+        this.y = y;
+    }
+    public void setZ(double z){
+        this.z = z;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+}
