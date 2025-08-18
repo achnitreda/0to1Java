29a64cefb4817fca4c5c2f51ced75a131965c183
achnitreda <achnitreda@gmail.com>
2025-08-18 15:13:43 +0100

    implements mutliple interfaces

---
+++ AdventureTemplar/Character.java
@@ -0,0 +1,86 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Character {
+    private final int maxHealth;
+    private int currentHealth;
+    private final String name;
+
+    private static List<Character> allCharacters = new ArrayList<>();
+
+    public int getMaxHealth() {
+        return this.maxHealth;
+    }
+
+    public int getCurrentHealth() {
+        return this.currentHealth;
+    }
+
+    public int setCurrentHealth(int currHealth) {
+        this.currentHealth = currHealth;
+        return this.currentHealth;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public Character(String name, int maxHealth) {
+        this.maxHealth = maxHealth;
+        this.name = name;
+        this.currentHealth = maxHealth;
+        allCharacters.add(this);
+    }
+
+    public static String printStatus() {
+        String border = "------------------------------------------";
+        String intro = "Characters currently fighting :";
+        String noChar = "Nobody's fighting right now !";
+        String res = border + "\n";
+        
+        if (allCharacters == null || allCharacters.isEmpty()) {
+            res += noChar + "\n" + border + "\n";
+            return res;
+        }
+        
+        res += intro + "\n";
+        for (Character c : allCharacters) {
+            res += " - " + c.toString() + "\n";
+        }
+        
+        return res + border + "\n";
+    }
+
+    public static Character fight(Character c1,Character c2) {
+        while (c1.getCurrentHealth() > 0 && c2.getCurrentHealth() > 0) {
+            c1.attack(c2);
+            if (c2.getCurrentHealth() > 0) {
+                c2.attack(c1);
+            }
+        }
+        if (c1.getCurrentHealth() > 0) {
+            return c1;
+        }else{
+            return c2;
+        }
+    }
+
+    @Override
+    public String toString() {
+        if (this.currentHealth <= 0) {
+            return String.format("%s : KO", this.name);
+        }
+        return String.format("%s : %d/%d", this.name,this.currentHealth,this.maxHealth);
+    }
+
+    public void takeDamage(int n) {
+        this.currentHealth -=n;
+        if (this.currentHealth < 0) {
+            this.currentHealth = 0;
+        }
+    }
+
+    public void attack(Character c) {
+        c.takeDamage(9);
+    }
+}
---
+++ AdventureTemplar/Templar.java
@@ -0,0 +1,36 @@
+public class Templar extends Character implements Healer, Tank {
+    private final int healCapacity;
+    private final int shield;
+
+    public Templar(String name, int maxHealth, int healCapacity, int shield) {
+        super(name, maxHealth);
+        this.healCapacity = healCapacity;
+        this.shield = shield;
+    }
+
+    @Override
+    public int getShield(){
+        return shield;
+    }
+
+    @Override
+    public int getHealCapacity(){
+        return healCapacity;
+    }
+
+    @Override
+    public void heal(Character c){
+        c.setCurrentHealth(c.getCurrentHealth()+healCapacity);
+        if (c.getCurrentHealth() > c.getMaxHealth()) {
+            c.setCurrentHealth(c.getMaxHealth());
+        }
+    }
+
+    @Override
+    public String toString() {
+        if (this.getCurrentHealth() <= 0) {
+            return String.format("%s has been beaten, even with its %d shield. So bad, it could heal %d HP.", this.getName(), this.shield, this.healCapacity);
+        }
+        return String.format("%s is a strong Templar with %d HP. It can heal %d HP and has a shield of %d.", this.getName(),this.getCurrentHealth(),this.healCapacity,this.shield);
+    } 
+}
