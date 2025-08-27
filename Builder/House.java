5ffd0af504ebc9ddf518246d69890a91d2b7fb46
achnitreda <achnitreda@gmail.com>
2025-08-27 18:49:38 +0100

    builder

---
+++ Builder/House.java
@@ -0,0 +1,29 @@
+class House implements Housing {
+    private int size; 
+    private int price;
+    private int rooms;
+    private String name;
+
+    House() {};
+
+    @Override
+    public void setSize(int size) {
+        this.size = size;
+    }
+    @Override
+    public void setPrice(int price) {
+        this.price = price;
+    }
+    @Override
+    public void setRooms(int rooms) {
+        this.rooms = rooms;
+    }
+    @Override
+    public void setName(String name) {
+        this.name = name;
+    }
+    @Override
+    public String toString() {
+        return String.format("House{size=%d, price=%d, rooms=%d, name='%s'}",size,price,rooms,name);
+    }
+}
