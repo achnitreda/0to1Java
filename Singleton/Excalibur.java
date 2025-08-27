9fd12e7319216148875c32f1c955ce1f69ab1f1d
achnitreda <achnitreda@gmail.com>
2025-08-27 17:23:17 +0100

    singleton

---
+++ Singleton/Excalibur.java
@@ -0,0 +1,21 @@
+public class Excalibur {
+    public static void main(String[] args)  {
+        System.out.println(Excalibur.getInstance().getName());
+    }
+
+    private String name;
+    private static Excalibur INSTANCE;
+
+    private Excalibur(String name) {
+        this.name = name;
+    }
+    public String getName() {
+        return name;
+    }
+    public static Excalibur getInstance() {
+        if (INSTANCE == null) {
+            INSTANCE = new Excalibur("Sword");
+        }
+        return INSTANCE;
+    }
+}
