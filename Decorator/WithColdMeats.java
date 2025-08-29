e40309bc98771934cf48d2ea996ef372fa0013a7
achnitreda <achnitreda@gmail.com>
2025-08-29 14:13:20 +0100

    Decorator

---
+++ Decorator/WithColdMeats.java
@@ -0,0 +1,16 @@
+public class WithColdMeats extends RacletteDecorator {
+    
+    public WithColdMeats(Raclette raclette) {
+        super(raclette);
+    }
+    
+    @Override
+    public int getCalories() {
+        return decoratedRaclette.getCalories() + 350;
+    }
+    
+    @Override
+    public String getIngredients() {
+        return decoratedRaclette.getIngredients() + ", charcuterie";
+    }
+}
---
+++ Decorator/WithPickles.java
@@ -0,0 +1,16 @@
+public class WithPickles extends RacletteDecorator {
+    
+    public WithPickles(Raclette raclette) {
+        super(raclette);
+    }
+    
+    @Override
+    public int getCalories() {
+        return decoratedRaclette.getCalories() + 50;
+    }
+    
+    @Override
+    public String getIngredients() {
+        return decoratedRaclette.getIngredients() + ", cornichons";
+    }
+}
